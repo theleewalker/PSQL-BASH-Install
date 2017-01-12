f50fe60cc5c3f433a2a9911310eb4e6713e54a0f
theleewalker <theleewalker@outlook.com>
2017-01-11 23:24:20 -0600

    Update psql-install.sh

--- psql-install.sh
+++ psql-install.sh
@@ -11,7 +11,7 @@
 # 6. Install PostgreSQL. ensuring the data files are stored in $dataFolder.
 # 7. Start the PostgreSQL service using the pg_ctl command.
 # 8. Run create_hello.sql script.
-# 9. Run '/usr/local/pgsql/bin/psql -c 'select * from hello;' -U test hello_postgres;' against newly created DB and test for succesful response.
+# 9. Run '/usr/local/pgsql/bin/psql -c 'select * from hello;' -U globus hello_postgres;' against newly created DB and test for succesful response.
 # 10. Add production enhancement, vacuuming is enabled by default so will not be added as a cron job, s.
 
 # Section 1 - Variable Creation
@@ -28,10 +28,10 @@ dfolder='/postgres/data'
 gitloc='git://git.postgresql.org/git/postgresql.git'
 # $sysuser is the system user for running PostgreSQL
 sysuser='postgres'
-# $helloscript is the sql script for creating the test user and creating a database.
+# $helloscript is the sql script for creating the Globus user and creating a database.
 helloscript='~/scripts/create_hello.sql'
 # $psqlcmd is the sql cmd that shows the content of the previously mentioned database.
-psqlcmd="/usr/local/pgsql/bin/psql -c 'select * from hello;' -U test hello_postgres;"
+psqlcmd="/postgres/bin/psql -c 'select * from hello;' -U globus hello_postgres;"
 
 # Section 2 - Package Installation
 
@@ -82,7 +82,7 @@ make >> psqlinstall-log
 echo "installing PSQL"
 sudo make install >> psqlinstall-log
 
-echo "Giving system user '$sysUser' over the $dfolder folder"
+echo "Giving system user '$sysuser' over the $dfolder folder"
 sudo chown postgres $dfolder >> psqlinstall-log
 
 # InitDB is used to create the location of the database cluster, for the purpose of this exercise it will be placed in the $dfolder under /db.
@@ -97,29 +97,120 @@ echo "Starting PSQL"
 sudo -u postgres $rfolder/bin/pg_ctl -D $dfolder/db -l $dfolder/logfilePSQL start >> psqlinstall-log
 
 # This block adds the environment variables for PostgreSQL to /etc/profile in order to set them for all users.
-echo "#Adding for PostgreSQL" | sudo tee -a /etc/profile >> psqlinstall-log
-echo "LD_LIBRARY_PATH=$rfolder/lib" | sudo tee -a /etc/profile >> psqlinstall-log
-echo "export LD_LIBRARY_PATH" | sudo tee -a /etc/profile >> psqlinstall-log
-echo "PATH=$rfolder/bin:$PATH" | sudo tee -a /etc/profile >> psqlinstall-log
-echo "export PATH" | sudo tee -a /etc/profile >> psqlinstall-log
+sudo cat >> /etc/profile <<EOL
+# PostgreSQL Environment Variables
 
+LD_LIBRARY_PATH=/postgres/lib
+export LD_LIBRARY_PATH
+PATH=/postgres/bin:$PATH
+export PATH
+EOL
 
-# The command to start PostgreSQL at launch is added to /etc/rc.local.
-echo "Setup PSQL to launch on startup"
-echo "sudo -u postgres /postgres/bin/pg_ctl -D /postgres/data/db -l /postgres/data/logfilePSQL start" | sudo tee -a /etc/rc.local >> psqlinstall-log
+# Section 8 - Add PostgreSQL to SystemD
+
+# Here we create the PostgreSQL init.d script wth the LSB block at the beginning
+sudo cat > /etc/init.d/postgresql <<EOL
+#!/bin/sh
+set -e
+
+### BEGIN INIT INFO
+# Provides:		postgresql
+# Required-Start:	$local_fs $remote_fs $network $time
+# Required-Stop:	$local_fs $remote_fs $network $time
+# Should-Start:		$syslog
+# Should-Stop:		$syslog
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	PostgreSQL RDBMS server
+### END INIT INFO
+
+# Installation prefix
+prefix=/postgres
+
+# Data directory
+PGDATA="/postgres/data"
+
+# Who to run the postmaster as, usually "postgres".  (NOT "root")
+PGUSER=postgres
+
+# Where to keep a log file
+PGLOG="$PGDATA/serverlog"
+
+# versions can be specified explicitly
+if [ -n "$2" ]; then
+    versions="$2 $3 $4 $5 $6 $7 $8 $9"
+else
+    get_versions
+fi
+
+case "$1" in
+    start|stop|restart|reload)
+        if [ "$1" = "start" ]; then
+            create_socket_directory
+        fi
+	if [ -z "`pg_lsclusters -h`" ]; then
+	    log_warning_msg 'No PostgreSQL clusters exist; see "man pg_createcluster"'
+	    exit 0
+	fi
+	for v in $versions; do
+	    $1 $v || EXIT=$?
+	done
+	exit ${EXIT:-0}
+        ;;
+    status)
+	LS=`pg_lsclusters -h`
+	# no clusters -> unknown status
+	[ -n "$LS" ] || exit 4
+	echo "$LS" | awk 'BEGIN {rc=0} {if (match($4, "down")) rc=3; printf ("%s/%s (port %s): %s\n", $1, $2, $3, $4)}; END {exit rc}'
+	;;
+    force-reload)
+	for v in $versions; do
+	    reload $v
+	done
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|reload|force-reload|status} [version ..]"
+        exit 1
+        ;;
+esac
+
+exit 0
+EOL
+
+sudo chmod 755 /etc/init.d/postgresql
+
+sudo cat > /etc/systemd/system/multi-user.target.wants/postgresql.service <<EOL
+# systemd service for managing all PostgreSQL clusters on the system. This
+# service is actually a systemd target, but we are using a service since
+# targets cannot be reloaded.
+
+[Unit]
+Description=PostgreSQL RDBMS
+
+[Service]
+Type=oneshot
+ExecStart=/bin/true
+ExecReload=/bin/true
+RemainAfterExit=on
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo chmod 777 /etc/systemd/system/multi-user.target.wants/postgresql.service
 
 # Section 8 - Globus hello.sql script is ran
 
 
-# This test script is ran to create the user, database, and populate the database.
-echo "Running test script"
-$rfolder/bin/psql -U postgres -f ~/scripts/hello.sql
+# The Globus script is ran to create the user, database, and populate the database.
+echo "Running Globus script"
+$rfolder/bin/psql -U postgres -f $helloscript
 
 # Section 9 - hello_postgres is queried
 
 
 echo "Querying the newly created table in the newly created database."
-$rfolder/bin/psql -c 'select * from hello;' -U user hello_postgres;
+$psqlcmd
 
 # Section 10 - Production improvements
 
